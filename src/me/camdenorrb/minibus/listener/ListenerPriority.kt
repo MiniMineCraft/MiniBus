ce09fdf1b3bcedb91139e9b2f69f154607617373
Camden <camdenorrb@me.com>
2017-03-22 09:11:04 -0500

    Init

--- src/me/camdenorrb/minibus/listener/ListenerPriority.kt
+++ src/me/camdenorrb/minibus/listener/ListenerPriority.kt
@@ -4,25 +4,15 @@ package me.camdenorrb.minibus.listener
  * Created by camdenorrb on 3/5/17.
  */
 
-enum class ListenerPriority(val order: Int) {
+enum class ListenerPriority {
 
-	FIRST(0), NORMAL(1), LAST(2);
-
-
-	fun byOrder(order: Int): ListenerPriority? {
-		return when(order) {
-			0 -> FIRST
-			1 -> NORMAL
-			2 -> LAST
-			else -> null
-		}
-	}
+	FIRST, NORMAL, LAST;
 
 
 	companion object {
 
 		val DEFAULT_COMPARATOR = Comparator<ListenerPriority> { o1, o2 ->
-			if (o1 == o2) 1 else o1.order.compareTo(o2.order)
+			if (o1 == o2) 1 else o1.ordinal.compareTo(o2.ordinal)
 		}
 
 	}
